faf3e83c2197b1f6c3fb8c4e1bfbb90bc6839ac4
Felix Everett <felixreverett@gmail.com>
2026-03-08 23:52:10 +0000

    Initial utils strings package

---
+++ utils/utils_strings.m
@@ -0,0 +1,50 @@
+|| ==========================================================================================================
+|| utils.strings
+|| ==========================================================================================================
+
+%export split startsWith toUpper toLower
+
+str == [char]
+
+|| ==========================================================================================================
+|| split
+|| > Splits a string into a list of string using a delimiter
+|| > Requires startsWith
+|| ==========================================================================================================
+
+split :: str -> str -> [str]
+
+split string delim
+  = reverse (xsplit string [] [])
+    where
+    xsplit [] acc substring = substring : acc
+    xsplit remaining acc substring
+      = xsplit (drop (#delim) remaining) (substring : acc) [], if startsWith remaining delim
+      = xsplit (tl remaining) acc (substring ++ [hd remaining]), otherwise
+
+|| ==========================================================================================================
+|| toUpper
+|| ==========================================================================================================
+
+toUpper = error "Not implemented exception"
+
+|| ==========================================================================================================
+|| toLower
+|| ==========================================================================================================
+
+toLower = error "Not implemented exception"
+
+|| ==========================================================================================================
+|| startsWith
+|| > Returns a boolean value to determine whether an input string starts with another
+|| > Note that I've defined startsWith [] any as False, and startsWith any [] as True.
+|| ==========================================================================================================
+
+startsWith :: str -> str -> bool
+startsWith string matcher
+  = xstartsWith string matcher
+    where
+    xstartsWith string [] = True
+    xstartsWith []     any = False
+    xstartsWith (x:xs) (x:ys) = xstartsWith xs ys
+    xstartsWith (x:xs) (y:ys) = False
