f545699905fa9284c5ce181cfad4fd2eaf9c705c
Felix Everett <felixreverett@gmail.com>
2026-03-10 14:51:56 +0000

    Added interactive gameplay to Soliteverett

--- solitaire/soliteverett.m
+++ solitaire/soliteverett.m
@@ -11,7 +11,7 @@
 
 
 
-|| ==========================================================================================================
+|| ======================================================================================
 || This is the Felix implementation of Solitaire, also known as 'Soliteverett'
 ||
 || Major Changes:
@@ -19,16 +19,16 @@
 || - Added dynamic boardsizing. This requires that the array not be jagged;
 || - You can play diagonally;
 || - Added a score to the accumulator. Yes you could just type `# moves`,
-||   but the score is incremented in the accumulator.
-|| - Added file loading and parsing of text into a pegboard.
+||   but the score is incremented in the accumulator;
+|| - Added file loading and parsing of text into a pegboard;
+|| - The game is now interactive!
 ||
 || Fixes:
 || - The initial implementation did not prevent off-board moves East or West;
 || - Added explicit error checking to ensure moves cannot land on an invalid tile,
 ||   which was previously handled incidentally by ensuring the destination was a Hole.
 ||
-|| ==========================================================================================================
-
+|| ======================================================================================
 
 || The pegstate type represents the three possible states of each cell on a board.
 || Invalid signifies an "out-of-bounds" cell to allow for non-rectangular boards.
@@ -46,9 +46,11 @@ southeast = ( 1,  1)
 southwest = (-1,  1)
 northwest = (-1, -1)
 
-|| Initial board state. This can be modified.
+|| Default board state. This can be modified.
 
-initboard
+pegboard == [[pegstate]]
+
+defaultBoard
   = [
       [Invalid, Invalid, Peg, Peg,  Peg, Invalid, Invalid],
       [Invalid, Invalid, Peg, Peg,  Peg, Invalid, Invalid],
@@ -63,56 +65,28 @@ initboard
 
 movement == (num, num, (num, num))
 
-|| the moveset for the game
-
-moves =
-  [
-    (4, 6, north),
-    (2, 5, east),
-    (3, 7, north),
-    (5, 4, southwest),
-    (4, 7, northwest)
-  ]
-
-|| ==========================================================================================================
-|| fn play
-|| ==========================================================================================================
-
-play :: [movement] -> [char]
-play moves
-  = printBoard (foldl f (initboard, 0) moves)
-    where
-    boardsizeX = # initboard
-    boardsizeY = # (initboard!0)
-    f (b, score) move = ((makemove.checkmove) (b, move), score + 1)
-    checkmove (b, (x , y, (xO, yO)))
-      = error "moved off board",          if ((x + xO) < 1)
-                                          \/ ((x + xO) > boardsizeX)
-                                          \/ ((y + yO)) < 1
-                                          \/ ((y + yO) > boardsizeY)
-      = error "moved off board (invalid)",if ((b!(y + 2 * yO - 1))!(x + 2 * xO - 1) = Invalid)
-      = error "no peg at given position", if ((b!(y - 1))!(x - 1) ~= Peg)
-      = error "no peg to jump over",      if ((b!(y +     yO - 1))!(x +     xO - 1) ~= Peg)
-      = error "no hole to jump into",     if ((b!(y + 2 * yO - 1))!(x + 2 * xO - 1) ~= Hole)
-      = (b, (x, y, (xO, yO))),            otherwise
-    makemove (b, (x, y, (xO, yO))) = ((make Peg (x + 2 * xO, y + 2 * yO)).(make Hole (x + xO, y + yO)).(make Hole (x, y))) b
-    make p (x, y) b = (take (y - 1) b) ++ [(makerow p x (b!(y - 1)))] ++ (drop y b)
-    makerow p x row = (take (x - 1) row) ++ [p] ++ (drop x row)
-
-|| ==========================================================================================================
+|| ======================================================================================
 || fn printBoard
-|| ==========================================================================================================
+|| Takes a board and score of type num, and returns a string
+|| ======================================================================================
 
-printBoard (b, score) 
-      = (lay (map (map g) b)) ++ "\nScore: " ++ show (score + 0)
+printBoard :: (pegboard, num) -> [char]
+printBoard (b, score)
+      = divider ++ spacer ++ (lay (map (map g) b)) ++ spacer ++ divider
+        ++ "\nScore: " ++ show (score + 0) ++ spacer ++ divider
         where
         g Peg = 'O'
         g Invalid = ' '
         g any = '_'
+        divider = ['=' | i <- [1..#(b!0)]]
+        spacer = "\n"
 
-|| ==========================================================================================================
+|| ======================================================================================
 || fn loadBoard
-|| ==========================================================================================================
+|| loads a board from a file given the filename.
+||
+|| NB: entering an invalid filename will crash the program.
+|| ======================================================================================
 
 loadBoard :: [char] -> [[pegstate]]
 loadBoard fileName
@@ -134,9 +108,9 @@ loadBoard fileName
     strToPegState "H" = Hole
     strToPegState any = error ("Unrecognised pegstate found in " ++ show (fileName) ++ ": " ++ show (any))
 
-|| ==========================================================================================================
+|| ======================================================================================
 || fn split
-|| ==========================================================================================================
+|| ======================================================================================
 
 split :: char -> [char] -> [[char]]
 
@@ -147,15 +121,105 @@ split splitter line
     rest = split splitter (drop 1 remainder), if #remainder > 0
          = [], otherwise
 
-|| ==========================================================================================================
+|| ======================================================================================
+|| fn playMove
+|| Takes a (board, score) tuple and movement, and returns an updated board and score.
+|| Replaces the earlier 'play' function, which simulated a whole moveset.
+|| ======================================================================================
+
+playMove :: (pegboard, num) -> movement -> (pegboard, num)
+playMove (board, score) move
+  = xPlayMove board score move
+    where
+    boardsizeX = # board
+    boardsizeY = # (board!0)
+    xPlayMove b s m = ((makemove.checkmove) (b, m), s + 1)
+    checkmove (b, (x , y, (xO, yO)))
+      = error "moved off board",          if ((x + xO) < 1)
+                                          \/ ((x + xO) > boardsizeX)
+                                          \/ ((y + yO)) < 1
+                                          \/ ((y + yO) > boardsizeY)
+      = error "moved off board (invalid)",if ((b!(y + 2 * yO - 1))!(x + 2 * xO - 1) = Invalid)
+      = error "no peg at given position", if ((b!(y - 1))!(x - 1) ~= Peg)
+      = error "no peg to jump over",      if ((b!(y +     yO - 1))!(x +     xO - 1) ~= Peg)
+      = error "no hole to jump into",     if ((b!(y + 2 * yO - 1))!(x + 2 * xO - 1) ~= Hole)
+      = (b, (x, y, (xO, yO))),            otherwise
+    makemove (b, (x, y, (xO, yO))) = ((make Peg (x + 2 * xO, y + 2 * yO)).(make Hole (x + xO, y + yO)).(make Hole (x, y))) b
+    make p (x, y) b = (take (y - 1) b) ++ [(makerow p x (b!(y - 1)))] ++ (drop y b)
+    makerow p x row = (take (x - 1) row) ++ [p] ++ (drop x row)
+
+|| ======================================================================================
+|| fn convertInputToMovement
+|| Converts a user input to a movement by splitting the input, casting the first
+|| two values into numbers and the third into a 'direction'
+||
+|| NB: There is no input validation here.
+|| ======================================================================================
+
+convertInputToMovement :: [char] -> movement
+convertInputToMovement input
+  = (x, y, setDir dir) 
+    where
+    splitInput = split ' ' input
+    x = numval (splitInput!0)
+    y = numval (splitInput!1)
+    dir = splitInput!2
+    setDir d
+     = north,     if d = "north"     \/ d = "n"
+     = east,      if d = "east"      \/ d = "e"
+     = south,     if d = "south"     \/ d = "s"
+     = west,      if d = "west"      \/ d = "w"
+     = northeast, if d = "northeast" \/ d = "ne"
+     = southeast, if d = "southeast" \/ d = "se"
+     = southwest, if d = "southwest" \/ d = "sw"
+     = northwest, if d = "northwest" \/ d = "nw"
+     = error "Invalid move!", otherwise
+
+|| ======================================================================================
+|| fn runGame
+|| The primary function to run the interactive game. Contains several helper functions
+|| which share the input stream: getBoardName; doGameLoop
+|| ======================================================================================
+
+runGame
+  = Stdout msg_init : getBoardName $-
+    where msg_init = "Welcome to Soliteverett!\nPlease enter a file name to load a board: "
+
+    || getBoardName :: [char] -> [sys_message]
+    getBoardName input
+      = Stdout msg_board : doGameLoop (board, 0) rest False
+        where
+        boardName = filter (~= '\r') (takewhile (~= '\n') input)
+        rest = drop 1 (dropwhile (~= '\n') input)
+
+        board = defaultBoard, if #boardName = 0
+              = loadBoard boardName, otherwise
+
+        msg_board = "Selected board: " ++ boardName ++ ".\n", if #boardName > 0
+                  = "No board selected. Using Default.\n", if #boardName = 0
+
+    || doGameLoop :: (pegboard, num) -> [char] -> bool -> [sys_message]
+    doGameLoop (currentBoard, score) input isGameOver
+      = [Stdout (printBoard (currentBoard, score) ++ "\nGame Over!")], if isGameOver
+      = Stdout prompt : processInput input, otherwise
+        where
+        prompt = "\n" ++ (printBoard (currentBoard, score))
+                      ++ "\nEnter your move (or 'q' to quit): "
+
+        processInput stream
+          = [Stdout "\nThanks for playing! Goodbye.\n"], if moveStr = "q"
+          = doGameLoop nextState rest False, otherwise
+            where
+            moveStr = filter (~= '\r') (takewhile (~= '\n') stream)
+            rest = drop 1 (dropwhile (~= '\n') stream)
+
+            parsedMove = convertInputToMovement moveStr
+            nextState = playMove (currentBoard, score) parsedMove
+
+|| ======================================================================================
 || fn main
-|| ==========================================================================================================
+|| ======================================================================================
 
-main :: [char]
-main = play moves
+main = runGame
 
-|| main = printBoard ((loadBoard "pegboard.txt"),0)
-|| main = printBoard ((loadBoard "diamondboard.txt"),0)
-
-
-|| ==========================================================================================================
+|| ======================================================================================
