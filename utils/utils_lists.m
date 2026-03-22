dd077e975b78a0daa382b85a1105d3feb0ecf94f
Felix Everett <felixreverett@gmail.com>
2026-03-22 12:41:58 +0000

    Add buildIndexers function to fold over a 2D List

--- utils/utils_lists.m
+++ utils/utils_lists.m
@@ -15,3 +15,15 @@ isInBounds :: vector2D -> [[*]] -> bool
 isInBounds (r, c) array
   = True,  if (r >= 0 & r < #array & c >= 0 & c < #(array!r))
   = False, otherwise
+
+|| ==========================================================================================================
+|| fn buildIndexers
+||
+|| > Build a list of indexers using list comprehensions, specifically for a 2D array
+||
+|| > NB: supports jagged arrays
+|| ==========================================================================================================
+
+buildIndexers :: [[*]] -> [vector2D]
+buildIndexers array
+  = [(a, b) | a <- [0..#array-1]; b <- [0..#(array!a)-1]]
