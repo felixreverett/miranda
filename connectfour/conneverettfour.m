e11036b2e4ab242539c6ed0571c9a1b1a5acec42
Felix Everett <felixreverett@gmail.com>
2026-04-04 18:27:19 +0100

    progress on conneverettfour

--- connectfour/conneverettfour.m
+++ connectfour/conneverettfour.m
@@ -4,7 +4,20 @@
 || the range of the board, outside of which the move is
 || invalid.
 
-movesList = ["4", "3", "2"]
+|| ========================================================== ||
+|| Game types below:                                          ||
+|| ========================================================== ||
+
+|| This moveslist should result in the second player winning.
+|| [char] has been chosen as the datatype over char because
+|| this more correctly maps to the input format from a
+|| command-line version of the game. 
+
+movesList
+  = ["3", "3", "2", "4",
+     "1", "0", "2", "2",
+     "3", "1", "3", "2",
+     "2", "1"]
 
 defaultBoard
   = ["_______",
@@ -15,8 +28,15 @@ defaultBoard
      "_______"
     ]
 
-|| gameOutcome is used 
-gameOutcome ::= Winner | Stalemate | Ongoing
+|| The 'gameState' is calculated after every move of the game
+|| to determine if a 'player' has won the game. The player
+|| is represented by a char collected by determining who made
+|| the most recent move. While this data can be collected
+|| separately to the gameState, I have chosen to include it
+|| within the algebraic type to improve readability of the
+|| code.
+
+gameState ::= Winner char | Stalemate | Ongoing
 
 || ========================================================== ||
 
@@ -24,11 +44,22 @@ main = run
 
 || ========================================================== ||
 
-run = printBoard (updateBoard defaultBoard 3 3 'X')
+run
+  = playAllMoves movesList
+  || printBoard (updateBoard defaultBoard 3 3 'X')
+
+|| ========================================================== ||
+
+playAllMoves :: [[char]] -> [char]
+playAllMoves [] = error "please enter a move"
+playAllMoves (x : xs) = playMove x
 
 || ========================================================== ||
 
 || processUserInput takes a user input and
+
+processUserInput
+  = error "not implemented exception"
 
 || ========================================================== ||
 
@@ -45,7 +76,7 @@ switchUser any = error "Unknown user. How did we get here?"
 || isGameWon is called after every turn to see if the win
 || condition has
 
-isGameWon :: [[char]] -> gameOutcome
+isGameWon :: [[char]] -> gameState
 isGameWon board
 
 || ========================================================== ||
